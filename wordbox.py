654c08c767b9f8ccf5fff6718068430e4525caba
eytterman <erik.ytterman@phaseshift.se>
2014-12-15 13:32:33 +0100

    New approach...

--- wordbox.py
+++ wordbox.py
@@ -1,106 +1,54 @@
 #! /usr/bin/python3
 import sys
 import random
+import itertools
 import copy
 import re
 
-from functools import reduce
-
-# Contain playfield state
-class playstate:
-    data = None
-    size = (0,0)
-
-    def __init__(self, rows, columns):
-        self.size = (rows, columns)
-        self.data = [ [ '.' for i in range(columns) ] for j in range(rows) ]
-
-    def getsize(self):
-        return self.size
-
-    def setrow(self, word, index):
-        self.data[index] = list(word)
-
-    def getrow(self, index):
-        return ''.join(self.data[index])
-
-    def getkeys(self):
-        return [ ''.join(t) for t in tuple(zip(*self.data)) ]
-
-    def dumptext(self, index = None, indent = 0):
-        if index == None:
-            for row in self.data:
-                print(indent * " " + ''.join(row))
-        else:
-            print(self.data[index])
-
-##########################################################################################
-
-count = 5
-
-normalizer = lambda line: line.strip().upper()
-
-worddata = [ normalizer(word) for word in open("swedish-word-list.txt", encoding="ISO-8859-1") ]
-
-wordlist = tuple( { word for word in sorted(worddata) if len(word) == count } )
-
-state = []
-choice = []
-
-for row in range(3):
-    print(10 * 'R' + 40 * str(row))
-
-    sample = random.choice(wordlist)
-    choice.append(list(sample))
-
-    for c in choice:
-        print(c)
-
-    print(10 * 'S' + 40 * str(row))
-
-    L = [ (i, ''.join(column), tuple([ w for w in wordlist if w.startswith(''.join(column)) ]) ) for i, column in enumerate(zip(*choice)) ]
-    
-    for i,l in enumerate(L): 
-        print(10 * "C" + 40 * str(i))
-        print(len(l[2]), l)
-
-    print(10 * 'R' + 40 * str(row))
-
-'''
 class treenode:
     state = None
     children = []
-
+    
     def __init__(self, state):
         self.state = state
 
-def buildtree(level, node):
-    for j, word in enumerate(wordlist):
-        if(level < count):
-            state = playstate(count, count)
-            state.data = copy.deepcopy(node.state.data)
-            state.size = copy.copy(node.state.size)
+def treebuilder(node, maxdepth):
+    if(len(node.state) == maxdepth):
+        print("SOLUTION")
+        for row in node.state:
+            print(row)
+        print("SOLUTION")
+    
+    for word in wordlist:
+        matches = []
+        for i, j in itertools.zip_longest(list(zip(*node.state)), list(zip(word)), fillvalue = ''):
+            column = '%s%s' % (''.join(i),''.join(j)) 
+            words = len([ w for w in wordlist if w.startswith(column) ])
+            matches.append(words)
+                  
+        if not 0 in matches:
+            print(20 * 'v')
 
-            state.setrow(word, level)
-            
-            results = [ [ w for w in wordlist if re.match(key, w) ] for key in state.getkeys() ]
-        
-            if not [] in results:
-                print("Level: " + str(level))
-                state.dumptext()
-                newnode = treenode(state)
-                node.children.append(newnode)
-                buildtree(level+1, newnode)
-        else:
-            print("SOLUTION!")
-            node.state.dumptext()
+            newstate = copy.deepcopy(node.state)
+            newstate.append(word)
+         
+            newnode = treenode(newstate)
 
-        if( j % 100 == 0 ):
-            sys.stdout.write('+')
-            sys.stdout.flush()
+            print("PARTIAL")
+            for row in newnode.state:
+                print(row)
+            print("PARTIAL")
 
-state = playstate(count, count)
-root = treenode(state)
+            node.children.append(newnode)
+            treebuilder(newnode, maxdepth)
+    
+    print(20 * '^')
 
-buildtree(0, root)
-'''
+maxdepth = 5
+normalizer = lambda line: line.strip().upper()
+worddata = [ normalizer(word) for word in open("swedish-word-list.txt", encoding="ISO-8859-1") ]
+wordlist = tuple( { word for word in sorted(worddata) if len(word) == maxdepth } )
+root = treenode([])
+
+treebuilder(root, maxdepth)
+
