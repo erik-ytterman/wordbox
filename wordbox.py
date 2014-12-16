938214a588ead1b5ba827bb82b6db6448a87750b
eytterman <erik.ytterman@phaseshift.se>
2014-12-16 16:34:06 +0100

    Initial working(?) implementation...

--- wordbox.py
+++ wordbox.py
@@ -10,45 +10,49 @@ class treenode:
     children = []
     
     def __init__(self, state):
-        self.state = state
+        self.state = list(state)
 
-def treebuilder(node, maxdepth):
-    if(len(node.state) == maxdepth):
-        print("SOLUTION")
+def charmatcher(word, charsets):
+    return ( False not in [ word[i] in charsets[i] for i in range(max(len(word), len(charsets))) ] )
+        
+def wordmatcher(state, wordset):
+    if state == []:
+        return wordset
+    else:
+        # Extract the possible words in the columns, filtered by the previous words
+        columnkeys = [ ''.join(t) for t in zip(*state) ]
+
+        # Extract possible words for every column
+        words = [ [ w for w in wordset if w.startswith(columnkey) ] for columnkey in columnkeys ]
+        
+        # Extract and "uniqify" possible letters per position in row to be selected
+        charsets = [ set([ l[len(state)] for l in word ]) for word in words ]
+
+        return { word for word in wordset if charmatcher(word, charsets) }
+
+def treebuilder(node, depth, maxdepth, wordset):
+    if depth < maxdepth:
+        for word in wordmatcher(node.state, wordset):
+            newnode = treenode(node.state)
+        
+            newnode.state.append(word)
+            
+            node.children.append(newnode)
+        
+            treebuilder(newnode, depth + 1, maxdepth, wordset)
+    else:
+        print(30 * '-')
+
         for row in node.state:
             print(row)
-        print("SOLUTION")
-    
-    for word in wordlist:
-        matches = []
-        for i, j in itertools.zip_longest(list(zip(*node.state)), list(zip(word)), fillvalue = ''):
-            column = '%s%s' % (''.join(i),''.join(j)) 
-            words = len([ w for w in wordlist if w.startswith(column) ])
-            matches.append(words)
-                  
-        if not 0 in matches:
-            print(20 * 'v')
 
-            newstate = copy.deepcopy(node.state)
-            newstate.append(word)
-         
-            newnode = treenode(newstate)
-
-            print("PARTIAL")
-            for row in newnode.state:
-                print(row)
-            print("PARTIAL")
-
-            node.children.append(newnode)
-            treebuilder(newnode, maxdepth)
-    
-    print(20 * '^')
 
 maxdepth = 5
 normalizer = lambda line: line.strip().upper()
 worddata = [ normalizer(word) for word in open("swedish-word-list.txt", encoding="ISO-8859-1") ]
-wordlist = tuple( { word for word in sorted(worddata) if len(word) == maxdepth } )
-root = treenode([])
+wordset = { word for word in sorted(worddata) if len(word) == maxdepth }
 
-treebuilder(root, maxdepth)
+root = treenode([])
+treebuilder(root, 0, maxdepth, wordset)
+
 
