f24303b974527cfdfbcdd4a87c552dd226240ce7
eytterman <erik.ytterman@phaseshift.se>
2015-03-04 11:42:09 +0100

    Major cleanup of solutoin finding algorithm...

--- wordbox.py
+++ wordbox.py
@@ -1,99 +1,88 @@
 #! /usr/bin/python3
 import sys
 
+# Configure
+maxdepth = columns = rows = 5
+
 class treenode:
-    state = None
-    parent = None
-    children = []
+   state = None
+   parent = None
+   children = []
     
-    def __init__(self, state, parent):
-        self.parent = parent
-        self.state = list(state)
+   def __init__(self, state, parent):
+      self.parent = parent
+      self.state = list(state)
 
-    def addword(self, word):
-        self.state.append(word)
+   def addword(self, word):
+      self.state.append(word)
 
-    def addchild(self, childnode):
-        self.children.append(childnode)
+   def addchild(self, childnode):
+      self.children.append(childnode)
 
-def charmatcher(word, charsets):
-    return ( False not in [ word[i] in charsets[i] for i in range(max(len(word), len(charsets))) ] )
+def rowfinder(state, wordset):
+    # Create keys from the columns of the present playfield 
+    columnkeys = [ ''.join(t) for t in zip(*state) ]
+
+    # Filter out the words, matching the key for each column
+    columnwords = [ [ word for word in wordset if word.startswith(columnkey) ] for columnkey in columnkeys ]
+
+    # Get the index of the row to be found (the playfield is a list of rows)
+    row = len(state)
+
+    # For every column in the playfield
+    for column in range(columns):
+        # Get the possible characters (charset) for every word position (column),
+        # derived from the possible column words (columnwords)
+        charset = { word[row] for word in columnwords[column] }
+
+        # Reduce the full wordset, finding words with a charcter in the present
+        # word position (column) matching the possible characters (charset) for
+        # this columnm, until ony valid words are left
+        wordset = { word for word in wordset if word[column] in charset }
+
+    return wordset
         
 def wordmatcher(state, wordset):
     if state == []:
         return wordset
     else:
-        # Extract the possible words in the columns, filtered by the previous words
-        columnkeys = [ ''.join(t) for t in zip(*state) ]
+        return rowfinder(state, wordset)
 
-        # Extract possible words for every column
-        words = [ [ w for w in wordset if w.startswith(columnkey) ] for columnkey in columnkeys ]
-        
-        # Extract and "uniqify" possible letters per position in row to be selected
-        charsets = [ set([ l[len(state)] for l in word ]) for word in words ]
-
-        return { word for word in wordset if charmatcher(word, charsets) }
-
-def treebuilder(node, depth, maxdepth, wordset, solutions, iterations = 0):
-    if depth == 1:
-        print("Progress: " + str(iterations / len(wordset)))
-        iterations += 1
-
+def treebuilder(node, depth, maxdepth, wordset, solutions):
     if depth < maxdepth:
         words = wordmatcher(node.state, wordset)
-
-        if debug:
-            print((depth * 3 * " ") + "Iterating over " + str(len(words)) + " on depth " + str(depth) + " in tree...", file=log)
 
         for word in words:
             newnode = treenode(node.state, node)
             newnode.addword(word)
             
             node.addchild(newnode)
-
-            if debug:
-                print((depth * 3 * " ") + word, file=log)
         
-            treebuilder(newnode, depth + 1, maxdepth, wordset, solutions, iterations)
+            treebuilder(newnode, depth + 1, maxdepth, wordset, solutions)
     else:
         solutions.append(node)
 
-        print("Solutions found: " + str(len(solutions)))
+        print("%d solutons found..." % len(solutions))
+
+#--------------------------------------------------------------
+# Main program
+#--------------------------------------------------------------
             
-        if debug:
-            print((depth * 3 * " ") + "Solution: " + str(len(solutions)), file=log)
-            for row in node.state:
-                print((depth * 3 * " ") + row, file=log)
-
-debug = False
-
 try:
-    if debug:
-        log = open("solution_trace.log", 'w')
-
-    maxdepth = 5
-    
     normalizer = lambda line: line.strip().upper()
+
     worddata = [ normalizer(word) for word in open("swedish-word-list.txt", encoding="ISO-8859-1") ]
-    wordset = { word for word in sorted(worddata) if len(word) == maxdepth }
+
+    wordset = { word for word in sorted(worddata) if len(word) == columns }
 
     solutions = []
+
     root = treenode([], None)
 
     treebuilder(root, 0, maxdepth, wordset, solutions)
-except KeyboardInterrupt:
-    out = open("solution_list.txt", 'w')
 
+except KeyboardInterrupt:
     for solution in solutions:
         print(30 * 'v')
         for row in solution.state:
             print(row)
-
-        print(30 * 'v', file=out)
-        for row in solution.state:
-            print(row, file=out)
-
-    out.close()
-finally:
-    if debug:
-        log.close()
