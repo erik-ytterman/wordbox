69401d8c046932963f7cab4dc2a892290b391323
eytterman <erik.ytterman@phaseshift.se>
2015-03-17 12:20:24 +0100

    Prepare to use different languages...

--- python/wordbox.py
+++ python/wordbox.py
@@ -175,7 +175,7 @@ def backtracker(node, track = []):
 # Helpers
 #--------------------------------------------------------------
 
-def loadwords(filename = "swedish-word-list.txt"):
+def loadwords(filename = r'../swedish-word-list.txt'):
    normalizer = lambda line: line.strip().upper()
    worddata = [ normalizer(word) for word in open(filename, encoding="ISO-8859-1") ]
    wordset = { word for word in sorted(worddata) if len(word) == columns }
