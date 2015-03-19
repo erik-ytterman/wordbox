68ae5b8658387d4cecd4a9ed7f00fc34d5ef31e9
eytterman <erik.ytterman@phaseshift.se>
2015-03-19 14:07:17 +0100

    Lots of ugly code to improve text report readability(?)

--- python/wordboxutils/reporting.py
+++ python/wordboxutils/reporting.py
@@ -1,25 +1,37 @@
 def printnode(node):
    # Print state
-   print(30 * str(node.row))
+   print(80 * str(node.row))
+   print()
    for row in node.state:
       print(row)
-      
+   print()
+
    # Print column keys and words
    if not node.columnkeys == None:
-      print(30 * "-")
+      print(80 * "-")
       for i, columnkey in enumerate(node.columnkeys):
-         if len(node.columnwords[i]) <= 7:
-            print("Key for column: %d is %s -> %s" % (i, columnkey, node.columnwords[i]))
+         if len(node.columnwords[i]) <= 5:
+            print("Columnkey.....%d is: %s -> %s" % (i, columnkey, node.columnwords[i]))
          else:
-            print("Key for column: %d is %s -> %s" % (i, columnkey, "MANY"))
+            print("Columnkey.....%d is: %s -> %s (%d)" % (i, columnkey, node.columnwords[i][0:4] + ["..."], len(node.columnwords[i])))
             
    if not node.charsets == None:
-      print(30 * "+")
+      print(80 * "-")
       for i, charset in enumerate(node.charsets):
-         print("Charset for column: %d is %s" % (i, charset))
-      if len(node.rowwords) <= 7:
-         print("Possible words: %s" % (node.rowwords))
-      else:
-         print("Possible words: %s" % ("MANY"))
+         if len(charset) <= 11:
+            print("Columncharset.%d is: %s" % (i, list(charset)))
+         else:
+            print("Columncharset.%d is: %s" % (i, list(charset)[0:10] + ['*']))
+      
+      print(80 * ".")
+      print()
 
-   print(30 * str(node.row))
+      wordcount = len(node.rowwords)
+      wordlist = [ word for word in node.rowwords ]
+
+      if wordcount <= 5:
+         print("Possible words: %s" % (wordlist))
+      else:
+         print("Possible words: %s (%d)" % (wordlist[0:4] + ["..."], wordcount))
+
+      print()
